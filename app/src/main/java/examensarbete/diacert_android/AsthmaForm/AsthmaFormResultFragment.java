7bc9c428fa35c42d540d28838f130dcb64ef58c2
Andreas Bäckevik <a.backevik@hotmail.com>
2016-04-19 08:52:11 +0200

    added asthma results

--- app/src/main/java/examensarbete/diacert_android/AsthmaForm/AsthmaFormResultFragment.java
+++ app/src/main/java/examensarbete/diacert_android/AsthmaForm/AsthmaFormResultFragment.java
@@ -1,9 +1,23 @@
 package examensarbete.diacert_android.AsthmaForm;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import examensarbete.diacert_android.R;
 
 /**
  * Created by backevik on 16-04-17.
  */
 public class AsthmaFormResultFragment extends Fragment {
+    private Bundle bundle;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.astma_form_view_q5, container, false);
+
+        return v;
+    }
 }
